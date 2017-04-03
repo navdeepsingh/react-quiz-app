66371ce1ce21df96c40cb4cbc352b216286f34af
Navdeep Singh <navdeep.er@gmail.com>
2017-04-03 12:46:06 +0530

    Structure created

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component{
+  render() {
+    return(
+      <div>APP</div>
+    )
+  }
+}
+
+module.exports = App;
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    filename: 'app/js/main.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      query:
+        {
+          presets:['es2015', 'react']
+        }
+    }]
+  }
+}
